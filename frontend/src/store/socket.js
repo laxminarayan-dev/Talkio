cf69689fdbb785620b95ea88b2038356a025433a
Laxmi Narayan <jaiswallucky0112@gmail.com>
2026-01-26 16:27:51 +0530

    Refactor socket connection to use origin URL

--- frontend/src/store/socket.js
+++ frontend/src/store/socket.js
@@ -4,17 +4,10 @@ import { io } from "socket.io-client";
 const backend_url = import.meta.env.VITE_BACKEND_URL;
 
 // Extract protocol + host (remove /talkio-backend)
-//const { origin } = new URL(backend_url);
-if(!backend_url){
-  console.log("nothing")
-}
-else{
-  console.log("something")
-}
+const { origin } = new URL(backend_url);
 
-const socket = io(backend_url, {
-  path: "/socket.io",
-  // path: "/talkio-backend/socket.io",
+const socket = io(origin, {
+  path: "/talkio-backend/socket.io",
   autoConnect: false,
   transports: ["websocket"],
   pingInterval: 25000,
