7daa673e94b69d1d5534729013cf5d27e0de683c
Laxmi Narayan <jaiswallucky0112@gmail.com>
2026-01-26 15:55:18 +0530

    Update socket.js

--- frontend/src/store/socket.js
+++ frontend/src/store/socket.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 
 const backend_url = import.meta.env.VITE_BACKEND_URL;
 
+console.log("In socket.js backend_url is : ",backend_url)
 const socket = io(backend_url, {
     autoConnect: false,
     transports: ["websocket"], // helps prevent polling issues on Render
