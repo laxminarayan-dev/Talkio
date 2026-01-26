be12d76af4035edecb86b4759e73ce9456133623
Laxmi Narayan <jaiswallucky0112@gmail.com>
2026-01-26 16:02:16 +0530

    Update socket.js

--- frontend/src/store/socket.js
+++ frontend/src/store/socket.js
@@ -3,12 +3,16 @@ import { io } from "socket.io-client";
 
 const backend_url = import.meta.env.VITE_BACKEND_URL;
 
-console.log("In socket.js backend_url is : ",backend_url)
-const socket = io(backend_url, {
-    autoConnect: false,
-    transports: ["websocket"], // helps prevent polling issues on Render
-    pingInterval: 25000,
-    pingTimeout: 60000,
+// Extract protocol + host (remove /talkio-backend)
+const { origin } = new URL(backend_url);
+
+const socket = io(origin, {
+  path: "/talkio-backend/socket.io",
+  autoConnect: false,
+  transports: ["websocket"],
+  pingInterval: 25000,
+  pingTimeout: 60000,
 });
+
 
 export default socket;
