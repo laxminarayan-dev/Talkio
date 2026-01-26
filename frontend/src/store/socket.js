0b3110615dabe3b8b577a81b054c728db0d210c5
Laxmi Narayan <jaiswallucky0112@gmail.com>
2026-01-26 16:05:57 +0530

    Update socket path for backend connection

--- frontend/src/store/socket.js
+++ frontend/src/store/socket.js
@@ -4,10 +4,11 @@ import { io } from "socket.io-client";
 const backend_url = import.meta.env.VITE_BACKEND_URL;
 
 // Extract protocol + host (remove /talkio-backend)
-const { origin } = new URL(backend_url);
+//const { origin } = new URL(backend_url);
 
 const socket = io(origin, {
-  path: "/talkio-backend/socket.io",
+  path: "/socket.io",
+  // path: "/talkio-backend/socket.io",
   autoConnect: false,
   transports: ["websocket"],
   pingInterval: 25000,
